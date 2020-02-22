30b1e76e0651a9dad0f376a2d50766b3b51da550
Jeneen <jeneen.lemar@gmail.com>
2020-02-22 01:09:09 -0500

    adds associations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,8 @@
 class Event < ApplicationRecord
+  belongs_to :user
+  has_many :user_events
+  belongs_to :category
+
+  # has_many :attending_users, through: :user_events -SETUP AND FIX WITH DANI
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: true
 
+  has_many :events
+  has_many :user_events
+
+  # has_many :attending_events, through: :user_events - SETUP AND FIX WITH DANI
+
 end
