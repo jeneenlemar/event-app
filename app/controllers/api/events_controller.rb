e52a5f91f31068f7ccd964d2cbff773136a536fe
Jeneen <jeneen.lemar@gmail.com>
2020-02-29 22:28:06 -0500

    adds current user restrictions to updating event

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -20,8 +20,12 @@ class Api::EventsController < ApplicationController
       img_url: params["img_url"],
       slots: params["slots"]
     )
-    @event.save
-    render "show.json.jb"
+    if @event.save
+      render "show.json.jb"
+    else
+      render json: { errors: @event.errors.full_messages }, status: :bad_request
+    end
+    
   end
 
   def show
@@ -31,18 +35,29 @@ class Api::EventsController < ApplicationController
 
   def update
     @event = Event.find(params[:id])
-    @event.title = params[:title] || @event.title
-    @event.short_description = params[:short_description] || @event.short_description
-    @event.details = params[:details] || @event.details
-    @event.time_start = params[:time_start] || @event.time_start
-    @event.duration = params[:duration] || @event.duration
-    @event.category_id = params[:category_id] || @event.category_id
-    @event.kit_price = params[:kit_price] || @event.kit_price
-    @event.location_description = params[:location_description] || @event.location_description
-    @event.address = params[:address] || @event.address
-    @event.img_url = params[:img_url] || @event.img_url
-    @event.slots = params[:slots] || @event.slots
-    render "show.json.jb" 
+    if @event.user_id == current_user.id
+      @event.title = params[:title] || @event.title
+      @event.short_description = params[:short_description] || @event.short_description
+      @event.details = params[:details] || @event.details
+      @event.time_start = params[:time_start] || @event.time_start
+      @event.duration = params[:duration] || @event.duration
+      @event.category_id = params[:category_id] || @event.category_id
+      @event.kit_price = params[:kit_price] || @event.kit_price
+      @event.location_description = params[:location_description] || @event.location_description
+      @event.address = params[:address] || @event.address
+      @event.img_url = params[:img_url] || @event.img_url
+      @event.slots = params[:slots] || @event.slots
+      if @event.save
+        render "show.json.jb"
+      else
+        render json: { errors: @event.errors.full_messages }, status: :bad_request
+      end
+    else
+      render json: {message: "Permission denied. You do not have the authority to update this information"}
+    end
+
+    
+     
   end
 
   def destroy
