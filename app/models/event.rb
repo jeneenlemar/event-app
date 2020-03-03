ce986c5b1aad6ae36892e6936e00e01757343ce9
Jeneen <jeneen.lemar@gmail.com>
2020-03-03 15:33:18 -0500

    removes orphaned data and adds destroy logic to dependents of User and Event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :user
-  has_many :user_events
+  has_many :user_events, dependent: :destroy
   belongs_to :category
 
   has_many :attendees, through: :user_events, source: :user
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :email, presence: true, uniqueness: true
 
-  has_many :events
+  has_many :events, dependent: :destroy
   has_many :user_events
 
   has_many :attending_events, through: :user_events, source: :event
