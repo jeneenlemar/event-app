d978a346d330d29a451412d9a73c6a179f6cbe51
Jeneen <jeneen.lemar@gmail.com>
2020-03-25 21:56:30 -0500

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,21 @@
 
 
-Event.create!([
-  {title: "Nature Bracelet", short_description: "Beading bracelet with natural and earthy design", details: "This is a beginner level project and no experience is needed.  We will create 1 bracelet during the class and the kit will provide materials to make 1 additional bracelet. You will be able to choose beads of various design. Beads are more wood and natural looking.  Intended for casual wear. Clasping closure. Wire cutters, pliers and other basic equipment will be on hand to share.", time_start: "2020-03-30 23:00:00", duration: 120.0, category_id: 3, kit_price: "35.0", location_description: "Lima VFW", address: "124 E. Elm St Lima, OH", user_id: 22, img_url: "https://res.cloudinary.com/jlemar/image/upload/v1585083471/events/beading_nature_bracelet_avo8cb.png", slots: 5},
-  {title: "Romantic Scrapbooking", short_description: "Scrapbooking pages for a Romantic theme", details: "This is a beginner level project and no experience is needed.  We will create 2 pages  during the class and the kit will provide materials to make 3 additional pages. You will be able to choose from various card stock colors and designs. You may bring beads, glitter and other accessories as desired. Punches, vinyl/paper cutter and other basic equipment will be on hand to share.", time_start: "2020-04-07 23:00:00", duration: 120.0, category_id: 8, kit_price: "30.0", location_description: "Lima Eagles Lodge", address: "800 Robb Ave. Lima, OH", user_id: 23, img_url: "https://res.cloudinary.com/jlemar/image/upload/v1585083486/events/scrapbook_romantic_cimejv.png", slots: 5},
-  
-])
-
-UserEvent.create!([
-  {event_id: 61, user_id: 9},
-  {event_id: 61, user_id: 10},
-  {event_id: 61, user_id: 14},
-  {event_id: 62, user_id: 19},
-  {event_id: 62, user_id: 22},
-  {event_id: 62, user_id: 25},
+# Event.create!([
+#   {title: "Summer Candles", short_description: "Summer Fragrance Candles", details: "This is a beginner level project and no experience is needed.  We will create 2 candles and the kit will provide materials to make an additional candle.   Basic equipment will be on hand to share.", time_start: "2020-04-06 23:00:00", duration: 160.0, category_id: 4, kit_price: "25.0", location_description: "Lima VFW", address: "124 E. Elm St Lima, OH", user_id: 2, img_url: "https://res.cloudinary.com/jlemar/image/upload/v1585083474/events/candle_summer_blend_t2rncr.png", slots: 4},
+#   {title: "Basic Bowl", short_description: "Clay basic bowl", details: "This is a beginner level project and no experience is needed.  We will create a basic bowl and the kit will provide materials.   Basic equipment will be on hand to share.", time_start: "2020-04-05 23:00:00", duration: 120.0, category_id: 5, kit_price: "20.0", location_description: "Lima Eagles Lodge", address: "800 Robb Ave. Lima, OH", user_id: 22, img_url: "https://res.cloudinary.com/jlemar/image/upload/v1585083479/events/pottery_mini_planter_yizdqt.png", slots: 5}
 
   
-])
+# ])
+
+# UserEvent.create!([
+  
+  
+#   {event_id: 85, user_id: 23},
+#   {event_id: 85, user_id: 25},
+#   {event_id: 85, user_id: 32},
+#   {event_id: 86, user_id: 23},
+#   {event_id: 86, user_id: 25},
+#   {event_id: 86, user_id: 32},
+  
+  
+# ])
