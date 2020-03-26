50f26972ce47a86525f994420437258ff3dd5695
Jeneen <jeneen.lemar@gmail.com>
2020-03-26 12:46:09 -0500

    fixes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,21 @@
 
 
-# Event.create!([
-#   {title: "Summer Candles", short_description: "Summer Fragrance Candles", details: "This is a beginner level project and no experience is needed.  We will create 2 candles and the kit will provide materials to make an additional candle.   Basic equipment will be on hand to share.", time_start: "2020-04-06 23:00:00", duration: 160.0, category_id: 4, kit_price: "25.0", location_description: "Lima VFW", address: "124 E. Elm St Lima, OH", user_id: 2, img_url: "https://res.cloudinary.com/jlemar/image/upload/v1585083474/events/candle_summer_blend_t2rncr.png", slots: 4},
-#   {title: "Basic Bowl", short_description: "Clay basic bowl", details: "This is a beginner level project and no experience is needed.  We will create a basic bowl and the kit will provide materials.   Basic equipment will be on hand to share.", time_start: "2020-04-05 23:00:00", duration: 120.0, category_id: 5, kit_price: "20.0", location_description: "Lima Eagles Lodge", address: "800 Robb Ave. Lima, OH", user_id: 22, img_url: "https://res.cloudinary.com/jlemar/image/upload/v1585083479/events/pottery_mini_planter_yizdqt.png", slots: 5}
+Event.create!([
+  {title: "Wooden Hearts", short_description: "Cut and Carved Wodden Hearts", details: "This is a beginner level project and no experience is needed.  We will create 3 wooden hearts and the kit will provide materials to make an additional heart.   Tools for cutting will be provided.  Tools for carving come in the kit. Basic equipment will be on hand to share.", time_start: "2020-04-02 23:00:00", duration: 160.0, category_id: 4, kit_price: "25.0", location_description: "Lima VFW", address: "124 E. Elm St Lima, OH", user_id: 23, img_url: "https://res.cloudinary.com/jlemar/image/upload/v1585083492/events/woodworking_hearts_n8rbjt.png", slots: 4},
+  {title: "Floral Cross Stitch", short_description: "Cross stich of bouquet of flowers", details: "This is a beginner level project and no experience is needed.  We will use a basic pattern and the kit has all thread, needle, and hoop.   Basic equipment will be on hand to share.", time_start: "2020-04-08 23:00:00", duration: 120.0, category_id: 11, kit_price: "20.0", location_description: "Lima Eagles Lodge", address: "800 Robb Ave. Lima, OH", user_id: 25, img_url: "https://res.cloudinary.com/jlemar/image/upload/v1585083495/events/stitching_flowers_baqd8c.png", slots: 5}
 
   
-# ])
+])
 
-# UserEvent.create!([
+UserEvent.create!([
   
   
-#   {event_id: 85, user_id: 23},
-#   {event_id: 85, user_id: 25},
-#   {event_id: 85, user_id: 32},
-#   {event_id: 86, user_id: 23},
-#   {event_id: 86, user_id: 25},
-#   {event_id: 86, user_id: 32},
+  {event_id: 87, user_id: 10},
+  {event_id: 87, user_id: 14},
+  {event_id: 87, user_id: 19},
+  {event_id: 88, user_id: 10},
+  {event_id: 88, user_id: 14},
+  {event_id: 88, user_id: 19},
   
   
-# ])
+])
